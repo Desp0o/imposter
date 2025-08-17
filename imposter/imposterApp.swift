25311e7ae2b601194ede3c44be07a4b75da16cb0
desp0o <tornike.despotashvili@gmail.com>
2025-08-17 23:06:34 +0400

    init

---
+++ imposter/imposterApp.swift
@@ -0,0 +1,17 @@
+//
+//  imposterApp.swift
+//  imposter
+//
+//  Created by Tornike Despotashvili on 8/17/25.
+//
+
+import SwiftUI
+
+@main
+struct imposterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
