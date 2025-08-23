7e272e2f4b46bf789a4b78b08bf09509ea5ef876
desp0o <tornike.despotashvili@gmail.com>
2025-08-23 20:03:37 +0400

    add geo rules

--- imposter/Views/Main/RulesView.swift
+++ imposter/Views/Main/RulesView.swift
@@ -9,16 +9,25 @@
 import SwiftUI
 
 struct RulesView: View {
+  @AppStorage("appLanguage") private var appLanguage: LanguageEnum = .ka
   @Binding var isRulesSheetVisible: Bool
   
-  let rulesArray: [String] = [
+  let rulesEng: [String] = [
     "Gather 3–5 friends and pass the phone around.",
     "Each player swipes to see the secret word — except one person, who will see \"Imposter.\"",
     "One by one, players say a word related to the secret word.",
     "The imposter must fake it and try to blend in without knowing the word.",
     "Keep giving clues and talking until someone thinks they've figured it out.",
     "When you're ready, vote for who you think the imposter is — then tap to reveal the truth!",
-    "Record the chaos! Share your funniest rounds on social media and tag your friends."
+  ]
+  
+  let rulesGeo: [String] = [
+      "შეიკრიბეთ 3–5 მეგობარი და გადაეცით ტელეფონი ერთმანეთს.",
+      "თითოეული მოთამაშე გადაატარებს თითს, რათა ნახოს საიდუმლო სიტყვა — გარდა ერთი ადამიანისა, რომელიც ნახავს „ჯაშუშს“ (Imposter).",
+      "ერთმანეთის მიყოლებით, მოთამაშეები ამბობენ საიდუმლო სიტყვასთან დაკავშირებულ სიტყვას.",
+      "თაღლითმა უნდა მოატყუოს და შეეცადოს შეერწყას სხვებს სიტყვის ცოდნის გარეშე.",
+      "გააგრძელეთ მინიშნებების მიცემა და საუბარი, სანამ ვინმე არ იფიქრებს, რომ მიხვდა.",
+      "როდესაც მზად იქნებით, ხმა მიეცით იმას, ვინც თქვენი აზრით, თაღლითია — შემდეგ შეეხეთ სიმართლის გამოსავლენად!"
   ]
   
   var body: some View {
@@ -39,8 +48,8 @@ struct RulesView: View {
       
       ScrollView {
         VStack(alignment: .leading, spacing: 20) {
-          ForEach(rulesArray.indices, id: \.self) { index in
-            let rule = rulesArray[index]
+          ForEach(appLanguage == .ka ? rulesEng.indices : rulesGeo.indices, id: \.self) { index in
+            let rule = appLanguage == .ka ? rulesGeo[index] : rulesEng[index]
             
             HStack(alignment: .top, spacing: 10) {
               Text("\(index + 1)")
