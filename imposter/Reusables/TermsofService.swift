92d66bfeac67e5d1c894b86341914c29ff972704
desp0o <tornike.despotashvili@gmail.com>
2025-08-30 15:20:32 +0400

    create privacy policy

--- imposter/Reusables/TermsofService.swift
+++ imposter/Reusables/TermsofService.swift
@@ -23,18 +23,22 @@ struct TermsofService: View {
                 კეთილი იყოს თქვენი მობრძანება Imposter-ში! ჩვენი მიზანია შემოგთავაზოთ სახალისო და მიმზიდველი თამაში. ამ წესებისა და პირობების (შემდგომში "პირობები") გამოყენებით, თქვენ ეთანხმებით მათ. გთხოვთ, ყურადღებით წაიკითხოთ ისინი.
               
               **1. ხელშეკრულების მიღება**
+              
                 ამ აპლიკაციის (შემდგომში "აპლიკაცია") ჩამოტვირთვის, ინსტალაციის ან გამოყენების გზით, თქვენ ეთანხმებით ამ პირობებს. თუ არ ეთანხმებით ამ პირობებს, არ უნდა გამოიყენოთ აპლიკაცია.
               
               **2. მომსახურების მიწოდება**
+              
                 Imposter არის თამაში, რომელიც მოიცავს შემდეგ კატეგორიებს:
               
               - **უფასო კატეგორიები:** სამი კატეგორია ხელმისაწვდომია უფასოდ.
               - **პრემიუმ კატეგორიები:** დამატებითი კატეგორიები ხელმისაწვდომია ფასიანი გამოწერით.
               
               **3. რეკლამები**
+              
                 აპლიკაციაში შეიძლება გამოჩნდეს გუგლის ბანერების სტილის რეკლამები, რათა მხარი დაუჭიროს უფასო კონტენტს.
               
               **4. გამოწერა**
+              
                 გამოწერის სარგებელი: გამოწერის შეძენით, მომხმარებლები იღებენ წვდომას ყველა კატეგორიაზე და აპლიკაციიდან ქრება ყველა რეკლამა.
               
               **გამოწერის ფასები:**
@@ -54,9 +58,11 @@ struct TermsofService: View {
               - ნებისმიერი მავნე ან ვირუსის შემცველი კოდის გადასაცემად.
               
               **6. ცვლილებები პირობებში**
+              
                 ჩვენ ვიტოვებთ უფლებას, ნებისმიერ დროს შევცვალოთ ეს პირობები ჩვენი შეხედულებისამებრ. თუ ცვლილება არსებითია, ჩვენ შეგატყობინებთ ცვლილების ძალაში შესვლამდე მინიმუმ 30 დღით ადრე. რას წარმოადგენს არსებითი ცვლილება, განისაზღვრება ჩვენი შეხედულებისამებრ.
               
-              **6. კონტაქტი**
+              **7. კონტაქტი**
+              
                თუ გაქვთ შეკითხვები ამ პირობების შესახებ, გთხოვთ, დაგვიკავშირდეთ: Tornike.despotashvili@itdc.ge
               """)
           .customFontSytle()
@@ -65,18 +71,22 @@ struct TermsofService: View {
                         Welcome to Imposter! Our goal is to offer you a fun and engaging game. By using these terms and conditions (hereinafter "Terms"), you agree to them. Please read them carefully.
                       
                       **1. Acceptance of Agreement**
+                      
                         By downloading, installing, or using this application (hereinafter "App"), you agree to these Terms. If you do not agree to these Terms, you must not use the App.
                       
                       **2. Service Provision**
+                      
                         Imposter is a game that includes the following categories:
                       
                       - **Free Categories:** Three categories are available for free.
                       - **Premium Categories:** Additional categories are available through a paid subscription.
                       
                       **3. Advertisements**
+                      
                         The App may display Google banner-style advertisements to support free content.
                       
                       **4. Subscription**
+                      
                         Subscription Benefits: By purchasing a subscription, users gain access to all categories, and all advertisements are removed from the App.
                       
                       **Subscription Prices:**
@@ -89,6 +99,7 @@ struct TermsofService: View {
                       - **Subscription Management:** You can manage your subscription and turn off auto-renewal in your device's account settings after purchase.
                       
                       **5. Prohibited Uses**
+                      
                       You agree not to use the App:
                       
                       - In any way that violates any applicable law or regulation.
@@ -96,14 +107,15 @@ struct TermsofService: View {
                       - To transmit any harmful or virus-containing code.
                       
                       **6. Changes to Terms**
+                      
                         We reserve the right, at our sole discretion, to modify these Terms at any time. If a revision is material, we will provide at least 30 days' notice prior to any new terms taking effect. What constitutes a material change will be determined at our sole discretion.
                       
-                      **6. Contact Us**
+                      **7. Contact Us**
+                      
                        If you have any questions about these Terms, please contact us: Tornike.despotashvili@itdc.ge
                       """)
           .customFontSytle()
         }
-        
       }
       .padding()
     }
@@ -115,3 +127,4 @@ struct TermsofService: View {
 #Preview {
   TermsofService()
 }
+
--- imposter/Views/Settings/Components/PrivacyPolicyView.swift
+++ imposter/Views/Settings/Components/PrivacyPolicyView.swift
@@ -9,14 +9,97 @@
 import SwiftUI
 
 struct PrivacyPolicyView: View {
+  @AppStorage("appLanguage") private var appLanguage: LanguageEnum = .ka
+
   var body: some View {
     ScrollView {
-      VStack(spacing: 20) {
-        Text("Pricacy Policy")
-          .customFontSytle(color: .mainWhite, weight: .bold, size: 20)
-        
-        Text(SettingsEnum.privacyPolicyText.rawValue)
+      Text("Pricacy Policy")
+        .customFontSytle(color: .mainWhite, weight: .bold, size: 20)
+        .padding(.top, 20)
+      
+      VStack() {
+        switch appLanguage {
+        case .ka:
+          Text("""
+                      [თქვენი კომპანიის სახელი] ("ჩვენ", "ჩვენი" ან "ჩვენ") ოპერირებს Imposter მობილურ აპლიკაციას (შემდგომში "სერვისი").
+                      
+                      ეს გვერდი გაცნობებთ ჩვენს პოლიტიკას პერსონალურ მონაცემებთან დაკავშირებით, როდესაც იყენებთ ჩვენს სერვისს.
+                      
+                      **1. მონაცემთა შეგროვება და გამოყენება**
+                      
+                      ჩვენ გვჯერა, რომ თქვენი კონფიდენციალურობა უმნიშვნელოვანესია. შესაბამისად, Imposter-ის გამოყენებისას:
+                      
+                      - **ჩვენ არ ვაგროვებთ არანაირ პერსონალურ საიდენტიფიკაციო ინფორმაციას** (როგორიცაა თქვენი სახელი, ელფოსტის მისამართი, ტელეფონის ნომერი ან ნებისმიერი სხვა ინფორმაცია, რომლითაც შესაძლებელია თქვენი იდენტიფიცირება).
+                      - **ჩვენ არ ვაგროვებთ გეოლოკაციის მონაცემებს.**
+                      - **ჩვენ არ ვაგროვებთ თქვენი მოწყობილობის შესახებ ინფორმაციას, გარდა ზოგადი გამოყენების მონაცემებისა**, რომლებიც აუცილებელია აპლიკაციის ფუნქციონირებისთვის და რომლებიც არ უკავშირდება თქვენს პირად იდენტობას.
+                      - **ჩვენ არ ვთხოვთ, არ ვაგროვებთ და არ ვინახავთ რაიმე სახის მონაცემებს ჩვენი მომხმარებლებისგან.**
+                      
+                      ჩვენი სერვისის მიზანია შემოგთავაზოთ გართობა თქვენი კონფიდენციალურობის დარღვევის გარეშე. თქვენი მონაცემები რჩება თქვენს მოწყობილობაზე და არ გადაეცემა ჩვენს სერვერებს ან მესამე მხარეს.
+                      
+                      **2. ქუქი-ფაილები და თვალთვალის ტექნოლოგიები**
+                      
+                      ჩვენ არ ვიყენებთ ქუქი-ფაილებს.
+                      
+                      **3. სერვისის პროვაიდერები**
+                      
+                      ჩვენ არ ვიყენებთ მესამე მხარის სერვისებს, რომლებსაც ექნებათ წვდომა თქვენს პერსონალურ მონაცემებზე, რადგან ჩვენ არ ვაგროვებთ ასეთ მონაცემებს. რეკლამები (როგორიცაა Google-ის ბანერები, როგორც მითითებულია ჩვენს Terms of Service-ში) შეიძლება გამოჩნდეს აპლიკაციაში და მათ შეიძლება ჰქონდეთ საკუთარი კონფიდენციალურობის პოლიტიკა, რომელზეც ჩვენ არ გვაქვს კონტროლი.
+                      
+                      **4. მონაცემთა უსაფრთხოება**
+                      
+                      მიუხედავად იმისა, რომ ჩვენ არ ვაგროვებთ პერსონალურ მონაცემებს, ჩვენ ვიღებთ ზომებს ჩვენი სერვისის უსაფრთხოების უზრუნველსაყოფად.
+                      
+                      **5. ამ კონფიდენციალურობის პოლიტიკის ცვლილებები**
+                      
+                      ჩვენ შეიძლება დროდადრო განვაახლოთ ჩვენი კონფიდენციალურობის პოლიტიკა. ჩვენ შეგატყობინებთ ნებისმიერი ცვლილების შესახებ ამ გვერდზე ახალი კონფიდენციალურობის პოლიტიკის განთავსებით.
+                      
+                      გირჩევთ, პერიოდულად გადახედოთ ამ კონფიდენციალურობის პოლიტიკას ნებისმიერი ცვლილებისთვის. ამ კონფიდენციალურობის პოლიტიკაში ცვლილებები ძალაში შედის ამ გვერდზე გამოქვეყნებისთანავე.
+                      
+                      **5. კონტაქტი**
+                      
+                      თუ გაქვთ შეკითხვები ამ კონფიდენციალურობის პოლიტიკის შესახებ, გთხოვთ, დაგვიკავშირდეთ: Tornike.despotashvili@itdc.ge
+                      """)
           .customFontSytle()
+        case .en:
+          Text("""
+                              [Your Company Name] ("we", "our", or "us") operates the Imposter mobile application (hereinafter "Service").
+                              
+                              This page informs you of our policies regarding personal data when you use our Service.
+                              
+                              **1. Data Collection and Use**
+                              
+                              We believe your privacy is paramount. Accordingly, when using Imposter:
+                              
+                              - **We do not collect any personally identifiable information** (such as your name, email address, phone number, or any other information that could identify you).
+                              - **We do not collect geolocation data.**
+                              - **We do not collect information about your device, other than general usage data** which is necessary for the App's functionality and is not linked to your personal identity.
+                              - **We do not request, collect, or store any type of data from our users.**
+                              
+                              Our Service aims to offer you entertainment without infringing on your privacy. Your data remains on your device and is not transmitted to our servers or third parties.
+                              
+                              **2. Cookies and Tracking Technologies**
+                              
+                              We do not use cookies.
+                              
+                              **3. Service Providers**
+                              
+                              We do not use third-party services that would have access to your personal data, as we do not collect such data. Advertisements (such as Google banners, as stated in our Terms of Service) may appear in the App, and they may have their own privacy policies over which we have no control.
+                              
+                              **4. Data Security**
+                              
+                              Although we do not collect personal data, we take measures to ensure the security of our Service.
+                              
+                              **5. Changes to This Privacy Policy**
+                              
+                              We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.
+                              
+                              You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.
+                              
+                              **5. Contact Us**
+                              
+                              If you have any questions about this Privacy Policy, please contact us: Tornike.despotashvili@itdc.ge
+                              """)
+          .customFontSytle()
+        }
       }
       .frame(maxWidth: .infinity, maxHeight: .infinity)
       .padding()
@@ -25,3 +108,8 @@ struct PrivacyPolicyView: View {
     .background(.mainBlack)
   }
 }
+
+#Preview {
+  PrivacyPolicyView()
+}
+
